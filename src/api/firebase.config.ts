7fb087f4d1d76d91bbe31636409231f4eca00d2d
Zoe Long <zeyulong0908@gmail.com>
2024-11-10 00:04:27 -0800

    11.9

--- src/api/firebase.config.ts
+++ src/api/firebase.config.ts
@@ -4,15 +4,25 @@ import { getAuth } from "firebase/auth";
 import dotenv from "dotenv";
 dotenv.config();
 
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: "fieldsodher.firebaseapp.com",
+//   projectId: "fieldsodher",
+//   storageBucket: "fieldsodher.firebasestorage.app",
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+//   databaseURL: "https://fieldsodher-default-rtdb.firebaseio.com",
+// };
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyCSoDmNSirUeZsDLnJ-H-rUzOWM4wLh5lA",
   authDomain: "fieldsodher.firebaseapp.com",
+  databaseURL: "https://fieldsodher-default-rtdb.firebaseio.com",
   projectId: "fieldsodher",
   storageBucket: "fieldsodher.firebasestorage.app",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  databaseURL: "https://fieldsodher-default-rtdb.firebaseio.com",
+  messagingSenderId: "1064243754593",
+  appId: "1:1064243754593:web:f0ee547b2b8871963c9c20",
+  measurementId: "G-GCM06HLCYQ",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
