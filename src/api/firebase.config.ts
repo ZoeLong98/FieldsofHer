8ad97dad5cba4dac0d3e55bf0e79c7dbab7c41f0
Zoe Long <zeyulong0908@gmail.com>
2024-11-10 00:02:51 -0800

    11.9

--- src/api/firebase.config.ts
+++ src/api/firebase.config.ts
@@ -4,16 +4,15 @@ import { getAuth } from "firebase/auth";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log(process.env.FIREBASE_API_KEY);
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  authDomain: "fieldsodher.firebaseapp.com",
+  projectId: "fieldsodher",
+  storageBucket: "fieldsodher.firebasestorage.app",
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  databaseURL: "https://fieldsodher-default-rtdb.firebaseio.com",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
